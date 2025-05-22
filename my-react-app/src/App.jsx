06b2a777b79f28a7c1470c2d43ebe4dd6069e006
DEEPBLUE104 <dlara7173@gmail.com>
2025-05-22 11:04:40 +0900

    서버

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import Exam3 from "./components/Exam3";
 import Exam4 from "./components/Exam4";
 import Exam5 from "./components/Exam5";
 import Exam6 from "./components/Exam6";
+import Exam7 from "./components/Exam7";
 
 function App() {
   // 상태(state)
@@ -31,7 +32,8 @@ function App() {
     // <Exam3 />
     // <Exam4 />
     // <Exam5 />
-    <Exam6 />
+    // <Exam6 />
+    <Exam7 />
   );
 }
 
